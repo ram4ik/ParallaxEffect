cf757fedeb2bcfe7b9602155f9bf44e84f92a235
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-11 22:09:59 +0300

    Initial Commit

---
+++ ParallaxEffect/ParallaxEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParallaxEffectApp.swift
+//  ParallaxEffect
+//
+//  Created by Ramill Ibragimov on 11.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct ParallaxEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
